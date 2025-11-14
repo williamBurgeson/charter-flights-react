9fa7bd5e930010b07d4031b3dbc09b6c44f72f81
william.burgeson <William.Burgeson@googlemail.com>
2025-11-14 22:24:49 +0100

    AirportSearchComponent

---
+++ src/components/AirportSearchComponent.tsx
@@ -0,0 +1,10 @@
+import './airport-search-component.css'
+
+export default function AirportSearchComponent() {
+  return (
+    <div className="airport-search-component">
+      <h2>Welcome to Airport Search</h2>
+      <p>This placeholder component will host the Airport Search UI.</p>
+    </div>
+  )
+}
--- src/pages/AirportSearchPage.tsx
+++ src/pages/AirportSearchPage.tsx
@@ -1,8 +1,10 @@
+import AirportSearchComponent from '../components/AirportSearchComponent'
+
 export default function AirportSearchPage() {
   return (
     <div className="table-box">
       <h1>Airport Search</h1>
-      <p>This is a placeholder page for the Airport Search feature. UI will be implemented later.</p>
+      <AirportSearchComponent />
     </div>
   )
 }
