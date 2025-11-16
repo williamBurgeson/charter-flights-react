548ead12ec8f78f8bf5a1d039d46167f009331b7
william.burgeson <William.Burgeson@googlemail.com>
2025-11-16 16:42:04 +0100

    delete some more dead files

--- src/components/airport-search/AirportSearchComponent.tsx
+++ src/components/airport-search/AirportSearchComponent.tsx
@@ -4,8 +4,6 @@ import AirportSearchTabPanelComponent from './AirportSearchTabPanelComponent'
 export default function AirportSearchComponent() {
   return (
     <div className="airport-search-component">
-      <h2>Welcome to Airport Search</h2>
-      <p>This placeholder component will host the Airport Search UI.</p>
           <AirportSearchTabPanelComponent />
     </div>
   )
