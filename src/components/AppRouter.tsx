9e84b1409f4a21addc2002acc7cf32b4afd280d2
william.burgeson <William.Burgeson@googlemail.com>
2025-11-16 13:30:44 +0100

    Stub flight search page

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,6 +3,7 @@ import HomePage from '../pages/HomePage'
 import MapPage from '../pages/MapPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import AirportSearchPage from '../pages/AirportSearchPage'
+import FlightSearchPage from '../pages/FlightSearchPage'
 import type { SuspenseResource } from '../utils/suspense-resource'
 import { EmptyPage } from '../pages/EmptyPage'
 
@@ -15,6 +16,7 @@ export default function AppRouter({ suspenseResource }: { suspenseResource: Susp
       <Route index element={<HomePage />} />
       <Route path="/map" element={<MapPage />} />
       <Route path="/airports" element={<AirportSearchPage />} />
+  <Route path="/flight-search" element={<FlightSearchPage />} />
       <Route path="*" element={<NotFoundPage />} />
       <Route path="/empty" element={<EmptyPage />} />
     </Routes>
---
+++ src/pages/FlightSearchPage.tsx
@@ -0,0 +1,8 @@
+export default function FlightSearchPage() {
+  return (
+    <div className="table-box">
+      <h2>Welcome to Flight Search</h2>
+      <p>Start by selecting search criteria on the left (placeholder).</p>
+    </div>
+  )
+}
