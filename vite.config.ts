671e0f7aab30d0e28e5d4508c189d3c1bb36f7aa
william.burgeson <William.Burgeson@googlemail.com>
2025-11-17 19:25:21 +0100

    more changes to fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './charter-flights-react/',
+  base: '/charter-flights-react',
   plugins: [
     react({
       babel: {
