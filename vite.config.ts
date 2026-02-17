06e295e9ff9b3c0fd56fac4f5593a3429b7a2519
William Burgeson <William.Burgeson@googlemail.com>
2026-02-17 21:16:10 +0100

    try again with setting the correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// not sure how to make this dynamic based on environment as it is a build time setting
-// maybe based on env variable? - but not important enough to tackle at the moment
 
-//const base = '/'
-const base = process.env.VITE_BASE_URL || '/'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: base,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    base: env.VITE_BASE_URL || '/',
+  }
 })
